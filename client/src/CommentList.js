cee35dd3596acb1d52b01400a8474227ea88a82d
Harshpreet Singh Wahla <harshpreet.wahla@elmosoftware.com.au>
2023-03-18 22:23:01 +1100

    use query service to fect post and comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,24 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default ({postId}) => {
-    const [comments, setComments] = useState({});
-
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    }
-
-    useEffect(() => {
-        fetchComments();
-    }, []);
-
+export default ({comments}) => {
+    
     const renderedComments = Object.values(comments).map(comments => {
-        return <li
-            key = {comments.id}
-            >
-             {comments.content}   
-
+        return <li key = {comments.id}>
+             {comments.content}
         </li>
     })
     return <ul >
