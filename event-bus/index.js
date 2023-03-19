7c23a4dcbd0917d7fa4d6ed51a3baa20a00bde7a
Harshpreet Singh Wahla <harshpreet.wahla@elmosoftware.com.au>
2023-03-19 17:25:00 +1100

    use the event data store

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,8 +7,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [] ;
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
+
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
   axios.post("http://localhost:4002/events", event);
@@ -16,6 +21,10 @@ app.post("/events", (req, res) => {
 
   res.send({ status: "OK" });
 });
+
+app.get("/events", (req,res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
